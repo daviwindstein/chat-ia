7bd43613f363e26edca0b1518872f05755ffa1a0
daviwindstein <daviwindsteinprozao@gmail.com>
2026-04-16 16:04:39 -0300

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,18 +1,32 @@
+function responder(msg) {
+  msg = msg.toLowerCase();
+
+  if (msg.includes("oi")) {
+    return "Oi 😄 tudo bem? Eu sou sua IA gamer 🎮🔥";
+  }
+
+  if (msg.includes("tudo bem")) {
+    return "Tudo ótimo! Melhor agora falando contigo 😎";
+  }
+
+  if (msg.includes("script")) {
+    return "Bora criar script 😈💻 me fala o que você quer!";
+  }
+
+  return "😅 Não entendi muito bem, mas tenta de novo aí!";
+}
+
 function enviar() {
   const input = document.getElementById("input");
   const chat = document.getElementById("chat");
 
-  const msg = input.value;
+  const texto = input.value;
 
-  chat.innerHTML += "<p><b>Você:</b> " + msg + "</p>";
+  chat.innerHTML += `<p><b>Você:</b> ${texto}</p>`;
 
-  let resposta = "😄 Oi! Não entendi muito bem, mas tô aqui!";
+  const resposta = responder(texto);
 
-  if (msg.includes("oi")) {
-    resposta = "Oi 😄 tudo bem?";
-  }
-
-  chat.innerHTML += "<p><b>IA:</b> " + resposta + "</p>";
+  chat.innerHTML += `<p><b>IA:</b> ${resposta}</p>`;
 
   input.value = "";
 }
