a6787063551166075678113ac984ee692b20ddf8
daviwindstein <daviwindsteinprozao@gmail.com>
2026-04-16 18:35:10 -0300

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,7 +1,6 @@
 const API_KEY = "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";
 
-async function perguntarIA(texto) {
-  try {
+
     const res = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
