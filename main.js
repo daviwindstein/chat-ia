e43735e503b7b1cf62d985bd5e1d30a0ab024291
daviwindstein <daviwindsteinprozao@gmail.com>
2026-04-16 16:04:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 npm init -y
 npm install electron
+
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
