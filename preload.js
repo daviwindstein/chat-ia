95bf4da94237cc3b8ac72cd54ff450de516bc791
daviwindstein <daviwindsteinprozao@gmail.com>
2026-04-16 16:57:35 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,13 +1,8 @@
-const fs = require('fs');
 const { contextBridge } = require('electron');
 const { exec } = require('child_process');
 
 contextBridge.exposeInMainWorld("api", {
-  criarArquivo: (nome, conteudo) => {
-    fs.writeFileSync(nome, conteudo);
-  },
-
   executar: (cmd) => {
-    exec(cmd);
+    if (cmd === "bloco") exec("notepad");
   }
 });
