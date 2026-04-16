f538605f69fbd882f7d740073373db44c1ba3f08
daviwindstein <daviwindsteinprozao@gmail.com>
2026-04-16 15:48:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    width: 1000,
+    height: 700
   });
 
   win.loadFile('index.html');
