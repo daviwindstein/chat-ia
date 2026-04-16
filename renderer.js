30c087f72b30470f4d7aa180908e335189d5caed
daviwindstein <daviwindsteinprozao@gmail.com>
2026-04-16 16:34:01 -0300

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -28,3 +28,18 @@ async function enviar() {
 
   input.value = "";
 }
+
+async function enviar() {
+  const input = document.getElementById("input");
+  const chat = document.getElementById("chat");
+
+  const texto = input.value;
+
+  chat.innerHTML += `<p><b>Você:</b> ${texto}</p>`;
+
+  const resposta = await perguntarIA(texto);
+
+  chat.innerHTML += `<p><b>IA:</b> ${resposta}</p>`;
+
+  input.value = "";
+}
