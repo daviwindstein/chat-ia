931479e2da77e91f989c0b27c530af2d4c5621a1
daviwindstein <daviwindsteinprozao@gmail.com>
2026-04-16 17:26:20 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -3,3 +3,12 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld("api", {
   perguntarIA: (texto) => ipcRenderer.invoke("ia", texto)
 });
+
+const { contextBridge } = require('electron');
+const fs = require('fs');
+
+contextBridge.exposeInMainWorld("pc", {
+  salvar: (nome, conteudo) => {
+    fs.writeFileSync(nome, conteudo);
+  }
+});
