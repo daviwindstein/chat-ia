445312ba2026bc1cafed0ff384a03dae09606549
daviwindstein <daviwindsteinprozao@gmail.com>
2026-04-16 16:22:41 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,8 +1,17 @@
 const { contextBridge } = require('electron');
 const { exec } = require('child_process');
 
+const comandosPermitidos = {
+  "abrir bloco": "notepad",
+  "abrir calculadora": "calc"
+};
+
 contextBridge.exposeInMainWorld("api", {
-  executar: (cmd) => {
-    exec(cmd);
+  executarSeguro: (nome) => {
+    if (comandosPermitidos[nome]) {
+      exec(comandosPermitidos[nome]);
+    } else {
+      alert("Comando bloqueado ⚠️");
+    }
   }
 });
